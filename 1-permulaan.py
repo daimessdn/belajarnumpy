ffccd0bdc0585bae73ed1e3350190f8a4c6a7904
daimessdn@gmail.com <daimessdn@gmail.com>
2021-07-08 14:35:26 +0700

    first commit with numpy overview setup

---
+++ 1-permulaan.py
@@ -0,0 +1,17 @@
+# import numpy library
+import numpy as np
+
+# inisiasi data
+a = np.array([1, 2, 3, 4, 5])
+b = [1, 2, 3, 4, 5]
+
+# print hasil
+print(a)
+print(b)
+
+# penambahan
+a = a + 1
+b = b + [1]
+
+print(a)
+print(b)
